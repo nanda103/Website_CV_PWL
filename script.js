1ac8f2d023a381a05c31284f5c9085b0681b0566
Winanda Sinurat <winandasinurat05@gmail.com>
2025-02-17 06:07:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contactButton = document.getElementById("contactButton");
+
+    contactButton.addEventListener("click", function() {
+        alert("Thank you for contacting me. You may reach me via email or phone number provided.");
+    });
+});
